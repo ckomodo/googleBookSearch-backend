5ec4461b0694d6d672f31155edadb74223db47db
ckomodo <chrissakwa@gmail.com>
2020-11-23 18:18:34 -0800

    fixing start script

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const db = require("./models");
 
 const PORT = process.env.PORT || 5000;
 
-// const cors = require("cors");
-// const User = require("./Models/userModel.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -15,53 +13,47 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(cors());
-
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+});
 
 //GET Book  route
 app.get("/books", (req, res) => {
-    db.Book.find()
-    .then(dbBook => {
+  db.Book.find()
+    .then((dbBook) => {
       res.json(dbBook);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 
 //POST book route
-app.post("/books", (req, res)=>{
-    db.Book.create(req.body).then(dbBook => {
-        res.json(dbBook);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-})
+app.post("/books", (req, res) => {
+  db.Book.create(req.body)
+    .then((dbBook) => {
+      res.json(dbBook);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 //DELETE route
 app.delete("/api/books/:id", (req, res) => {
-    db.Book.deleteOne(
-      
-      {_id: req.params.id}
-
-    )
-    .then(dbBook => {
+  db.Book.deleteOne({ _id: req.params.id })
+    .then((dbBook) => {
       res.json(dbBook);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontEnd/build"));
+  app.use(express.static("client/build"));
 }
-
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
